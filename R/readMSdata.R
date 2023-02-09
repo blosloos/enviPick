d346b23b1f7dc7a226c4e3a45b74fcc617e19c1b
blosloos <mloos@envibee.ch>
2023-02-09 20:04:00 +0100

    Update readMSdata.R

--- R/readMSdata.R
+++ R/readMSdata.R
@@ -12,18 +12,11 @@ function(
 	
 		##########################################################################
 		# checks #################################################################
-		if(!is.character(file.path(filepath.mzXML))){stop("filepath.mzML not a character string")}
-		if(!file.exists(file.path(filepath.mzXML))){stop("invalid filepath.mzML")}
-		if(minmz==FALSE){
-			min_mz<-0
-		}else{
-			min_mz<-minmz
-		}
-		if(maxmz==FALSE){
-			max_mz<-Inf
-		}else{
-			max_mz<-maxmz
-		}
+		if(!is.character(file.path(filepath.mzXML))) stop("filepath.mzML not a character string")
+		if(!file.exists(file.path(filepath.mzXML))) stop("invalid filepath.mzML")
+		if(!minmz) min_mz <- 0 else min_mz <- minmz
+		if(!maxmz) max_mz <- Inf else max_mz <- maxmz
+		
 		if(!is.logical(ion_mode)){
 			if((ion_mode!="positive") & (ion_mode!="negative") & (ion_mode!="any")){
 				stop("Wrong polarity argument")
@@ -33,15 +26,15 @@ function(
 	  
 		##########################################################################
 		# local function definition ##############################################
-		genMSlist<-function(){
+		genMSlist <- function(){
 			# define MSlist list #################################################
-			MSlist<-list(0);
-			MSlist[[2]]<-0;MSlist[[3]]<-0;MSlist[[4]]<-0;MSlist[[5]]<-0;MSlist[[6]]<-0;MSlist[[7]]<-0;MSlist[[8]]<-0;
-			names(MSlist)<-c("State","Parameters","Results","Scans","Partition_index","EIC_index","Peak_index","Peaklist")
-			stage<-data.frame(FALSE,FALSE,FALSE,FALSE,FALSE);
-			names(stage)<-c("Raw?","Partitioned?","Clustered?","Filtered?","Picked?");
-			MSlist[[1]]<-stage;
-			p<-c(   
+			MSlist <- list(0)
+			MSlist[[2]] <- 0; MSlist[[3]] <- 0; MSlist[[4]] <- 0; MSlist[[5]] <- 0; MSlist[[6]] <- 0; MSlist[[7]] <- 0; MSlist[[8]] <- 0;
+			names(MSlist) <-c("State","Parameters","Results", "Scans","Partition_index", "EIC_index", "Peak_index", "Peaklist")
+			stage <- data.frame(FALSE, FALSE, FALSE, FALSE, FALSE)
+			names(stage) <- c("Raw?", "Partitioned?", "Clustered?", "Filtered?","Picked?")
+			MSlist[[1]] <- stage;
+			p <- c(   
 			"agglom_dmzgap",#1
 			"agglom_ppm",
 			"agglom_drtgap",
@@ -77,23 +70,23 @@ function(
 			"pick_from",
 			"pick_to"#34	  
 			);   
-			parameters<-data.frame(p,rep("0",length(p)),stringsAsFactors=FALSE);
-			names(parameters)<-c("parameters","value")
-			MSlist[[2]]<-parameters;
+			parameters <-data.frame(p, rep("0", length(p)), stringsAsFactors = FALSE)
+			names(parameters) <- c("parameters", "value")
+			MSlist[[2]] <- parameters
 			######################################################################
 			return(MSlist)
 		}
 		##########################################################################
       
 		##########################################################################
-		mz1 <- readMzXmlData::readMzXmlFile(file.path(filepath.mzXML), removeMetaData = FALSE,verbose = FALSE)
-		peaknumb<-0
-		RT<-c()
-		done_cen<-FALSE
+		mz1 <- readMzXmlData::readMzXmlFile(file.path(filepath.mzXML), removeMetaData = FALSE, verbose = FALSE)
+		peaknumb <- 0
+		RT <- c()
+		done_cen <- FALSE
 		for(i in 1:length(mz1)){
-			if((any(mz1[[i]]$metaData$msLevel==MSlevel)) & (mz1[[i]]$metaData$peaksCount>0)){
-				if((minRT!=FALSE) & (mz1[[i]]$metaData$retentionTime < minRT)) next
-				if((maxRT!=FALSE) & (mz1[[i]]$metaData$retentionTime > maxRT)) next
+			if((any(mz1[[i]]$metaData$msLevel == MSlevel)) & (mz1[[i]]$metaData$peaksCount > 0)){
+				if((minRT != FALSE) & (mz1[[i]]$metaData$retentionTime < minRT)) next
+				if((maxRT != FALSE) & (mz1[[i]]$metaData$retentionTime > maxRT)) next
 				if(!is.logical(ion_mode)){
 					if((ion_mode == "positive") & (mz1[[i]]$metaData$polarity != "+")) next
 					if((ion_mode == "negative") & (mz1[[i]]$metaData$polarity != "-")) next
@@ -112,45 +105,43 @@ function(
 				peaknumb <- c(peaknumb + sum(mz1[[i]][[1]]$mass >= min_mz & mz1[[i]][[1]]$mass <= max_mz, na.rm = TRUE))
 			}
 		}
-		if(peaknumb == 0){
-			stop("\nWith this file & settings: no peaks available. Maybe a wrong ionization mode is specified?\n")
-		}
+		if(peaknumb == 0) stop("\nWith this file & settings: no peaks available. Maybe a wrong ionization mode is specified?\n")
 		scans <- list(0)
 		scans[[1]] <- RT      
         getpeaks <- matrix(nrow = peaknumb, ncol = 7, 0)
-		from <- 1;
+		from <- 1
 		if(progbar == TRUE){  
 			prog <- winProgressBar("Extract scans", min = 0, max = length(mz1));
             setWinProgressBar(prog, 0, title = "Extract scans", label = NULL);
 		}
 		for(i in 1:length(mz1)){
-			if(progbar==TRUE){setWinProgressBar(prog, i, title = "Extract scans", label = NULL);}
-			if(any(mz1[[i]]$metaData$msLevel==MSlevel) & (mz1[[i]]$metaData$peaksCount>0)){
-				if((minRT!=FALSE)&(mz1[[i]]$metaData$retentionTime<minRT)) next
-				if((maxRT!=FALSE)&(mz1[[i]]$metaData$retentionTime>maxRT)) next
+			if(progbar) setWinProgressBar(prog, i, title = "Extract scans", label = NULL) 
+			if(any(mz1[[i]]$metaData$msLevel == MSlevel) & (mz1[[i]]$metaData$peaksCount > 0)){
+				if((minRT != FALSE) & (mz1[[i]]$metaData$retentionTime < minRT)) next
+				if((maxRT != FALSE) & (mz1[[i]]$metaData$retentionTime > maxRT)) next
 				if(!is.logical(ion_mode)){
-					if((ion_mode=="positive")&(mz1[[i]]$metaData$polarity!="+")) next
-					if((ion_mode=="negative")&(mz1[[i]]$metaData$polarity!="-")) next
+					if((ion_mode == "positive") & (mz1[[i]]$metaData$polarity != "+")) next
+					if((ion_mode == "negative") & (mz1[[i]]$metaData$polarity != "-")) next
 				}
-				those<-which(mz1[[i]][[1]]$mass>=min_mz & mz1[[i]][[1]]$mass<=max_mz)
-				if(length(those)==0){next}
-				to<-(from+length(those)-1)
-				getpeaks[from:to,1]<-mz1[[i]][[1]]$mass[those]
-				getpeaks[from:to,2]<-mz1[[i]][[1]]$intensity[those]				
-				getpeaks[from:to,3]<-(mz1[[i]]$metaData$retentionTime)
-				from<-(to+1)	  
+				those <- which(mz1[[i]][[1]]$mass >= min_mz & mz1[[i]][[1]]$mass <= max_mz)
+				if(!length(those)) next
+				to <- (from + length(those) - 1)
+				getpeaks[from:to, 1] <- mz1[[i]][[1]]$mass[those]
+				getpeaks[from:to, 2] <- mz1[[i]][[1]]$intensity[those]				
+				getpeaks[from:to, 3] <- (mz1[[i]]$metaData$retentionTime)
+				from <- (to + 1)	  
 			}
 		}  
-		if(progbar==TRUE){close(prog)};
-		getpeaks[,4]<-seq(1,length(getpeaks[,1]),1)
-		colnames(getpeaks)<-c("m/z","intensity","RT","measureID","partID","clustID","peakID")
-		scans[[2]]<-getpeaks;
-		rm(getpeaks);
-		MSlist<-genMSlist();      
-		MSlist[[4]]<-scans;
+		if(progbar) close(prog)
+		getpeaks[, 4] <- seq(1, length(getpeaks[, 1]), 1)
+		colnames(getpeaks) <- c("m/z", "intensity", "RT", "measureID", "partID", "clustID", "peakID")
+		scans[[2]] <- getpeaks
+		rm(getpeaks)
+		MSlist <- genMSlist()    
+		MSlist[[4]] <- scans
 		rm(scans)
-		MSlist[[1]][[1]]<-TRUE
+		MSlist[[1]][[1]] <- TRUE
 		##########################################################################
-		return(MSlist);
+		return(MSlist)
 	      
 }
